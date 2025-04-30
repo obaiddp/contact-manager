fd9f54981718b74a1a29aae863ede9dc3c1b689c
obaiddp <obaidullahzeb38@gmail.com>
2025-04-30 23:29:52 +0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 // connect
-mongoose.connect('mongodb+srv://obaidullahzeb182:obaid123@cluster0.chrpiit.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect(MONGOOSE_URI);
 
 // create schema
 const UserSchema = new mongoose.Schema({
